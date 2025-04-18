6a563616726cf3b15b5e9f0f030322aa73569b4e
Caleb Blevins <blevsca2@gmail.com>
2025-04-17 21:42:06 -0400

    adding a js file to make for a better user experience

---
+++ script.js
@@ -0,0 +1,5 @@
+const openPopUp = document.getElementById("open-popup");
+const closePopUp = document.getElementById("close-popup") 
+const popup = document.getElementById("popup");
+const overlay = document.getElementById("overlay");
+
